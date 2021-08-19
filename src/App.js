0ab3a31088b95e35f8a38db072adebe4fe2d1496
farhan zaman <farhanzaman@farhans-MacBook-Pro.local>
2021-08-18 21:20:38 -0400

    calculator

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/Home';
+
+const App = () => {
+  return <Home />;
+};
+
+export default App;
